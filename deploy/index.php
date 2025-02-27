88002308616459dd19881681a4306c0e62cfee7a
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2025-02-27 13:23:07 +0900

    change to abs

--- deploy/index.php
+++ deploy/index.php
@@ -1,18 +1,16 @@
 <?php
 
-file_put_contents('~/logs/deploy3.log', '>>><<<', FILE_APPEND | LOCK_EX);
-
 if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     http_response_code(405);
     return;
 }
 
-$secret = file_get_contents('~/.envs/gh_secret');
+$secret = file_get_contents('/home/ugrad/24/s2410303/.envs/gh_secret');
 $body = file_get_contents('php://input');
 
 $signature = 'sha256=' . hash_hmac('sha256', $body, $secret);
 
-file_put_contents('~/logs/deploy2.log', '>>>' . $body . '<<<' . $signature . '>>>' . $_SERVER['HTTP_X-HUB-SIGNATURE-256'] . '<<<' . $_SERVER['HTTP_X-Hub-Signature-256'] . '>>>', FILE_APPEND | LOCK_EX);
+file_put_contents('/home/ugrad/24/s2410303/logs/deploy2.log', '>>>' . $body . '<<<' . $signature . '>>>' . $_SERVER['HTTP_X-HUB-SIGNATURE-256'] . '<<<' . $_SERVER['HTTP_X-Hub-Signature-256'] . '>>>', FILE_APPEND | LOCK_EX);
 
 if ($_SERVER['HTTP_X-HUB-SIGNATURE-256'] != $signature) {
     http_response_code(403);
@@ -20,7 +18,7 @@ if ($_SERVER['HTTP_X-HUB-SIGNATURE-256'] != $signature) {
 }
 
 $dec_body = json_decode($body, true);
-file_put_contents('~/logs/deploy.log', $body, FILE_APPEND | LOCK_EX);
+file_put_contents('/home/ugrad/24/s2410303/logs/deploy.log', $body, FILE_APPEND | LOCK_EX);
 
 if ($dec_body['action'] != 'push') {
     http_response_code(204);
@@ -37,4 +35,4 @@ if ($dec_body['ref'] != 'refs/heads/main') {
     return;
 }
 
-exec('~/pull_web.sh');
+exec('/home/ugrad/24/s2410303/pull_web.sh');
