b58353f8cca9900b8024c49ae51a710d15c30c7f
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2025-02-27 13:46:32 +0900

    change to header

--- deploy/index.php
+++ deploy/index.php
@@ -18,12 +18,7 @@ if ($_SERVER['HTTP_X_HUB_SIGNATURE_256'] != $signature) {
 $dec_body = json_decode($body, true);
 file_put_contents('/home/ugrad/24/s2410303/logs/deploy.log', $body, FILE_APPEND | LOCK_EX);
 
-if ($dec_body['action'] != 'push') {
-    http_response_code(204);
-    return;
-}
-
-if ($dec_body['action'] != 'push') {
+if ($_SERVER['HTTP_X_GitHub_Event'] != 'push') {
     http_response_code(204);
     return;
 }
@@ -34,3 +29,5 @@ if ($dec_body['ref'] != 'refs/heads/main') {
 }
 
 exec('/home/ugrad/24/s2410303/pull_web.sh');
+
+http_response_code(201);
