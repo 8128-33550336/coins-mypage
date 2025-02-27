d139b62fcf4af4f431000a824484813a46a59185
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2025-02-27 13:03:18 +0900

    log

--- deploy/index.php
+++ deploy/index.php
@@ -10,13 +10,15 @@ $body = file_get_contents('php://input');
 
 $signature = 'sha256=' . hash_hmac('sha256', $body, $secret);
 
+file_put_contents('~/logs/deploy2.log', '>>>' . $body . '<<<' . $signature . '>>>' . $_SERVER['HTTP_X-HUB-SIGNATURE-256'] . '<<<' . $_SERVER['HTTP_X-Hub-Signature-256'] . '>>>', FILE_APPEND | LOCK_EX);
+
 if ($_SERVER['HTTP_X-HUB-SIGNATURE-256'] != $signature) {
     http_response_code(403);
     return;
 }
 
 $dec_body = json_decode($body, true);
-file_put_contents('~/deploy.log', $body, FILE_APPEND | LOCK_EX);
+file_put_contents('~/logs/deploy.log', $body, FILE_APPEND | LOCK_EX);
 
 if ($dec_body['action'] != 'push') {
     http_response_code(204);
