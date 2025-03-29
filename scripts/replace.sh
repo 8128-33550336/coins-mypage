5de6e7a18b593c68c53f539f1695c141808c3dc9
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2025-03-29 15:14:57 +0900

    change scr

--- scripts/replace.sh
+++ scripts/replace.sh
@@ -3,5 +3,5 @@
 while read line; do
     key=`echo "$line" | cut -d '=' -f 1`
     val=`echo "$line" | cut -d '=' -f 2`
-    find ./public -type f | xargs -I F -n 1 sed -i "s|{$key}|$val|g" F
+    find ./public -type f | xargs sed -i "s|{$key}|$val|g"
 done < .env.template
