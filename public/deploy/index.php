f541c89083f064334f5b2a408aca6b906bbebd1b
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2025-03-29 16:45:50 +0900

    fix

--- public/deploy/index.php
+++ public/deploy/index.php
@@ -18,20 +18,16 @@ if ($_SERVER['HTTP_X_HUB_SIGNATURE_256'] != $signature) {
 $dec_body = json_decode($body, true);
 file_put_contents('/home/ugrad/24/s2410303/logs/deploy.log', $body, FILE_APPEND | LOCK_EX);
 
-if ($_SERVER['HTTP_X_GITHUB_EVENT'] != 'release') {
+if ($_SERVER['HTTP_X_GITHUB_EVENT'] != 'push') {
     http_response_code(204);
     return;
 }
 
-if ($dec_body['action'] != 'created') {
+if ($dec_body['ref'] != 'refs/heads/main') {
     http_response_code(204);
     return;
 }
 
-$tag_name = $dec_body['release']['tag_name'];
-
-print("Deploying {$tag_name}...\n");
-
-print(`/home/ugrad/24/s2410303/pull_web.sh "{$tag_name}" 2>&1`);
+print(`/home/ugrad/24/s2410303/pull_web.sh 2>&1`);
 
 http_response_code(201);
