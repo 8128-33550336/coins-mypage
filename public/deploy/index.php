e26c078a82eebbeeae3ce5a27a1e714625c001df
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2025-03-29 14:00:20 +0900

    fix deploy script

--- public/deploy/index.php
+++ public/deploy/index.php
@@ -18,16 +18,18 @@ if ($_SERVER['HTTP_X_HUB_SIGNATURE_256'] != $signature) {
 $dec_body = json_decode($body, true);
 file_put_contents('/home/ugrad/24/s2410303/logs/deploy.log', $body, FILE_APPEND | LOCK_EX);
 
-if ($_SERVER['HTTP_X_GITHUB_EVENT'] != 'push') {
+if ($_SERVER['HTTP_X_GITHUB_EVENT'] != 'release') {
     http_response_code(204);
     return;
 }
 
-if ($dec_body['ref'] != 'refs/heads/main') {
+if ($dec_body['action'] != 'created') {
     http_response_code(204);
     return;
 }
 
-print(`/home/ugrad/24/s2410303/pull_web.sh`);
+$tag_name = $dec_body['release']['tag_name'];
+
+print(`/home/ugrad/24/s2410303/pull_web.sh "$tag_name" 2>&1`);
 
 http_response_code(201);
