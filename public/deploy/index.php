e660fb900bc56fc3d0f6eb6aa80aa36d9cacab90
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2025-03-29 14:13:32 +0900

    fix dep

--- public/deploy/index.php
+++ public/deploy/index.php
@@ -30,6 +30,6 @@ if ($dec_body['action'] != 'created') {
 
 $tag_name = $dec_body['release']['tag_name'];
 
-print(`/home/ugrad/24/s2410303/pull_web.sh "$tag_name" 2>&1`);
+print(`/home/ugrad/24/s2410303/pull_web.sh "{$tag_name}" 2>&1`);
 
 http_response_code(201);
