34d52396194f411085c6a050a456fb6547b7c8f4
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2025-04-25 08:45:02 +0900

    custom 404

---
+++ root/lab/er/404.php
@@ -0,0 +1,17 @@
+<?php
+http_response_code(404);
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404</title>
+</head>
+
+<body>
+    this is custom 404 page.
+</body>
+
+</html>
